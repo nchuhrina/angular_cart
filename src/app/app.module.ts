841bf918f10ea91ed0ef2376f8643bf6f973c276
Nina <pinkangelssugar@gmail.com>
2020-07-23 08:12:41 +0300

    without routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ShoppingCartComponent } from './cart/cart.component';
+import {ProductComponent}  from './catalog/product.component';
+import {CartProductComponent} from './cart/cart.product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, CatalogComponent, ShoppingCartComponent, ProductComponent, CartProductComponent],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
